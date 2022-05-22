fcc55ba4803813327c86a9702cd11dec8d3792f8
tusar-verma <tusarverma12345@gmail.com>
2022-05-22 16:53:08 -0300

    Agregado algunos iconos a contacto

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,20 @@
+import React from 'react'
+import "./css/Contact.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons"
+
 
 function Contact() {
   return (
-    <>
-      <a href="https://github.com/tusar-verma" target={"_blank"} rel="noreferrer">Github</a>
-    </>
+    <div >
+      <a href="https://github.com/tusar-verma" target={"_blank"} rel="noreferrer">
+        <FontAwesomeIcon icon={faGithub} size="4x"/>
+        <FontAwesomeIcon icon={faLinkedin} size="4x"/>
+      </a>
+      <p></p>
+     
+    </div>
   )
 }
 
