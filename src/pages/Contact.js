99faee4b0736edea31901e40d009889a7490e5da
tusar-verma <tusarverma12345@gmail.com>
2022-05-22 13:59:27 -0300

    Elegi la paleta de colores y acomode mejor el css de mis proyectos

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,6 @@
 function Contact() {
   return (
     <>
-      <img src={require("../images/profile.jpg")} alt="Profile"/>
       <a href="https://github.com/tusar-verma" target={"_blank"} rel="noreferrer">Github</a>
     </>
   )
