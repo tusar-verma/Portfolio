f89ca33207a8fbb3ba92c1de04a6ab2ee1734115
tusar-verma <tusarverma12345@gmail.com>
2022-05-25 15:29:19 -0300

    Agregado configuracion para el deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import AboutMe from './pages/AboutMe';
 import Contact from './pages/Contact';
 import MyProjects from './pages/MyProjects';
@@ -7,15 +7,15 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>        
       <Routes>
         <Route path="/" element={<Navigation />}>
           <Route index element={<AboutMe />} />
           <Route path="MyProjects" element={<MyProjects />} />
           <Route path="Contact" element={<Contact />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
+      </Routes>      
+    </HashRouter>
     );
 }
 
