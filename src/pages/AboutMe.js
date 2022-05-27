7be567bfa34e9f6f85a217b47c774fe72c64de60
tusar-verma <tusarverma12345@gmail.com>
2022-05-26 22:07:20 -0300

    Corregido bug y terminado el diseño responsivo
    Corregido bug que hacia que desaparezca la barra de navegacion en pantallas menores a 600px
    retocado algunas cosas de css

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -5,11 +5,11 @@ function AboutMe() {
     <div className="aboutme">        
       <img id="profilepic" src={require("../images/profile.jpg")} alt="Profile"/>
       <p className="description">
-        Mi nombre es Tusar, tengo 20 años y actualmente estoy haciendo la carrera de Licuenciatura en ciencias de la computación
-        en la facultad de ciencias exactas y naturales de la UBA. Tambien me recibí de técnico en computación en la
-        escuela técnica n°3 María Sanchez de Thompson. <br></br><br></br>
+        Mi nombre es Tusar, tengo 20 años y actualmente estoy haciendo la carrera de Licenciatura en ciencias de la computación
+        en la facultad de ciencias exactas y naturales de la UBA. También me recibí de técnico en computación en la
+        escuela técnica N°3 María Sanchez de Thompson. <br></br><br></br>
         Me gusta la programación y aprender sobre ella, sigo el camino autodidacta además de la facultad. Hice varios cursos
-        sobre python, web development, git, actualmente me encuentro haciendo el curso de fullstack de codo a codo.<br></br> 
+        sobre python, web development y git. Actualmente me encuentro haciendo el curso de fullstack de codo a codo.<br></br> 
         No tengo experiencia laboral pero he hecho algunos proyectos en grupo con compañeros de estudio, con tecnologías como
         Java, Haskell, Mysql, Arduino, SQL server. Me gusta trabajar en equipo y siempre que encuentro una dificultad me esmero en
         buscar una solución.
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,10 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
 import "./css/Navigation.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from "@fortawesome/free-solid-svg-icons"
 
 const Navigation = () => {
+
   return (
     <>
       <nav>
+        <button id="btnMenu" onClick={toggleMenu}><FontAwesomeIcon icon={faBars} size="2x"/></button>
+
         <Link className="link" to="/">Sobre mi</Link>
         <Link className="link" to="/MyProjects">Mis proyectos</Link>
         <Link className="link" to="/Contact">Contacto</Link>
@@ -13,5 +18,18 @@ const Navigation = () => {
     </>
   )
 };
+
+function toggleMenu (e) {
+  e.preventDefault();
+  const menuLinks = document.getElementsByClassName('link')
+
+  // https://stackoverflow.com/questions/3871547/iterating-over-result-of-getelementsbyclassname-using-array-foreach
+  // getElementsByClassName devuelve un objeto de tipo HTMLCollection y no podes usar forEach
+  // con esto se "convierte" para poder usar el foreach
+  Array.prototype.forEach.call(menuLinks, function(menuLink) {   
+    menuLink.style.display = menuLink.style.display === "" ? "block" : ""
+  })
+    
+}
 
 export default Navigation;
